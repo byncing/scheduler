cb39b36dbd1c70c7a5aab63bcf166ece6f7c9b7b
byncing <byncing@gmail.com>
2021-11-22 01:26:32 +0100

    Update example

--- src/test/java/eu/byncing/scheduler/test/Test.java
+++ src/test/java/eu/byncing/scheduler/test/Test.java
@@ -1,11 +1,17 @@
 package eu.byncing.scheduler.test;
 
 import eu.byncing.scheduler.Scheduler;
+import eu.byncing.scheduler.pool.Pool;
 
 public class Test {
 
     public static void main(String[] args) {
         Scheduler scheduler = new Scheduler();
-        scheduler.runTimer(() -> System.out.println("tick scheduler"), 1000);
+        Pool pool = scheduler.pool(3);
+
+        scheduler.runTimer(() -> {
+            pool.execute(() -> System.out.println(Thread.currentThread().getName() + ": task run"));
+            pool.execute(() -> System.out.println(Thread.currentThread().getName() + ": task run"));
+        }, 0, 500);
     }
 }
